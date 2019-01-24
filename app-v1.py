b0f12e57dbd2937f53ad77d02ba7a3954613c6ae
rusti <mrgregory1@gmail.com>
2019-01-24 00:07:53 -0500

    finished implementing breakaway

--- app-v1.py
+++ app-v1.py
@@ -42,7 +42,7 @@ def setup():
     team_color = request.form["team_color"]
     session["round"] = 0
     session["current_hand"] = []
-    session["chosen_cards"] = []
+    session["choosen_cards"] = []
     # load cards from json data and add team colors
     with open("static/sprinter_cards.json", "r") as f:
         session["sprint_deck"] = json.load(f)
@@ -60,206 +60,13 @@ def setup():
     session["current_deck"] = ""
     session["is_sprint_exaust"] = False
     session["is_roll_exaust"] = False
-    # begin dealing with breakaway options
-    if "breakaway_option" in request.form:
-        session["breakaway_option"] = request.form["breakaway_option"]
-        session["is_breakaway_winner_0"] = False
-        session["is_breakaway_winner_1"] = False
-        session.modified = True
-        return render_template("breakaway_deck_choice.html")
-    session.modified = True
-    return redirect(url_for("choose_deck"))
-
-
-#
-# @app.route("/breakaway_deck_choice")
-# def breakaway_deck_choice():
-#     return render_template("breakaway_deck_choice.html")
-
-
-@app.route("/breakaway_picker_1/<chosen_breakaway_deck>", methods=["POST", "GET"])
-def breakaway_picker_1(chosen_breakaway_deck):
-    # chosen_deck = request.form["deck_choice"]
-    if chosen_breakaway_deck == "sprint":
-        random.shuffle(session["sprint_deck"])
-        for x in range(4):
-            current_card = session["sprint_deck"].pop()
-            session["current_hand"].append(current_card)
-    else:
-        random.shuffle(session["roll_deck"])
-        for x in range(4):
-            current_card = session["roll_deck"].pop()
-            session["current_hand"].append(current_card)
-    current_hand = session["current_hand"]
-    session["current_deck"] = chosen_breakaway_deck
-    if session["current_deck"] == "sprint":
-        deck_for_title = "Sprinter"
-    else:
-        deck_for_title = "Roller"
-    session.modified = True
-    return render_template(
-        "breakaway_picker_1.html",
-        current_hand=current_hand,
-        deck_for_title=deck_for_title,
-    )
-
-
-@app.route("/hidden_first_breakaway", methods=["POST", "GET"])
-def hidden_first_breakaway():
-    # get card choice from last page
-    chosen_card_position = request.form["card_choice"]
-    # assign that card choice to chosen card and add that to session list
-    chosen_card = session["current_hand"].pop(int(chosen_card_position))
-    session["chosen_cards"].append(chosen_card)
-
-    # add rest of hand to facedown cards
-    if session["current_deck"] == "sprint":
-        session["sprint_faceup"].extend(session["current_hand"])
-        session["current_hand"] = []
-        # session["sprint_discards"].append(chosen_card)
-    else:
-        session["roll_faceup"].extend(session["current_hand"])
-        session["current_hand"] = []
-        # session["roll_discards"].append(chosen_card)
-    current_deck = session["current_deck"]
-    session.modified = True
-    return render_template("breakaway_hidden_1.html", current_deck=current_deck)
-
-
-@app.route("/revealed_breakaway_card_1")
-def revealed_breakaway_card_1():
-    chosen_cards = session["chosen_cards"]
-    return render_template("revealed_breakaway_card_1.html", chosen_cards=chosen_cards)
-
-
-@app.route("/breakaway_picker_2")
-def breakaway_picker_2():
-    # chosen_deck = request.form["deck_choice"]
-    if session["current_deck"] == "sprint":
-        random.shuffle(session["sprint_deck"])
-        for x in range(4):
-            current_card = session["sprint_deck"].pop()
-            session["current_hand"].append(current_card)
-    else:
-        random.shuffle(session["roll_deck"])
-        for x in range(4):
-            current_card = session["roll_deck"].pop()
-            session["current_hand"].append(current_card)
-    current_hand = session["current_hand"]
-    if session["current_deck"] == "sprint":
-        deck_for_title = "Sprinter"
-    else:
-        deck_for_title = "Roller"
-    session.modified = True
-    return render_template(
-        "breakaway_picker_2.html",
-        current_hand=current_hand,
-        deck_for_title=deck_for_title,
-    )
-
-
-@app.route("/breakaway_hidden_2", methods=["POST", "GET"])
-def breakaway_hidden_2():
-    # get card choice from last page
-    chosen_card_position = request.form["card_choice"]
-    # assign that card choice to chosen card and add that to session list
-    chosen_card = session["current_hand"].pop(int(chosen_card_position))
-    session["chosen_cards"].append(chosen_card)
-
-    # add rest of hand to facedown cards
-    if session["current_deck"] == "sprint":
-        session["sprint_faceup"].extend(session["current_hand"])
-        session["current_hand"] = []
-        session["sprint_deck"].extend(session["sprint_faceup"])
-        session["sprint_faceup"] = []
-        # session["sprint_discards"].append(chosen_card)
-    else:
-        session["roll_faceup"].extend(session["current_hand"])
-        session["current_hand"] = []
-        session["roll_deck"].extend(session["roll_faceup"])
-        session["roll_faceup"] = []
-        # session["roll_discards"].append(chosen_card)
-    # current_deck = session["current_deck"]
-    revealed_card = session["chosen_cards"][0]
-    hidden_card = session["chosen_cards"][1]
-    current_deck = session["current_deck"]
-
-    session.modified = True
-    return render_template(
-        "breakaway_hidden_2.html",
-        revealed_card=revealed_card,
-        hidden_card=hidden_card,
-        current_deck=current_deck,
-    )
-
-
-@app.route("/breakaway_revealed_card_2")
-def breakaway_revealed_card_2():
-
-    # replace chosen cards with exhaustion
-    chosen_card_0 = session["chosen_cards"][0]
-    chosen_card_1 = session["chosen_cards"][1]
-    session.modified = True
-    return render_template(
-        "breakaway_revealed_card_2.html",
-        chosen_card_0=chosen_card_0,
-        chosen_card_1=chosen_card_1,
-    )
-
-
-@app.route("/breakaway_winner/<place>")
-def breakaway_winner(place):
-    place = int(place)
-    print(place)
-    print(type(place))
-    if session["current_deck"] == "sprint":
-        session["sprint_deck"].append([2, "S", "exhaustion-card"])
-        session["sprint_discards"].append(session["chosen_cards"][place])
-    else:
-        session["roll_deck"].append([2, "R", "exhaustion-card"])
-        session["roll_discards"].append(session["chosen_cards"][place])
-    if place == 0:
-        session["is_breakaway_winner_0"] = True
-    if place == 1:
-        session["is_breakaway_winner_1"] = True
-
-    session.modified = True
-    return redirect(url_for("breakaway_revealed_card_2"))
-
-
-@app.route("/breakaway_final_check")
-def breakaway_final_check():
-    breakaway_card_0 = session["chosen_cards"][0]
-    breakaway_card_1 = session["chosen_cards"][1]
-    print(breakaway_card_0)
-    print(breakaway_card_1)
-    print(session["is_breakaway_winner_0"])
-    print(session["is_breakaway_winner_1"])
-
-    if (
-        session["is_breakaway_winner_0"] == True
-        and session["is_breakaway_winner_1"] == True
-    ):
-        return redirect(url_for("choose_deck"))
-    if session["is_breakaway_winner_0"] == False:
-        print("inside 0 false loop")
-        if session["current_deck"] == "sprint":
-            session["sprint_deck"].append(breakaway_card_0)
-        else:
-            session["roll_deck"].append(breakaway_card_0)
-    if session["is_breakaway_winner_1"] == False:
-        print("inside 1 false loop")
-        if session["current_deck"] == "sprint":
-            session["sprint_deck"].append(breakaway_card_1)
-        else:
-            session["roll_deck"].append(breakaway_card_1)
     session.modified = True
     return redirect(url_for("choose_deck"))
 
 
 @app.route("/choose_deck")
 def choose_deck():
-    session["chosen_cards"] = []
+    session["choosen_cards"] = []
     session["round"] += 1
     session["is_sprint_exaust"] = False
     session["is_roll_exaust"] = False
@@ -316,10 +123,10 @@ def card_picker_1(chosen_deck):
 @app.route("/card_picker_2", methods=["POST", "GET"])
 def card_picker_2():
     # get card choice from last page
-    chosen_card_position = request.form["card_choice"]
+    choosen_card_position = request.form["card_choice"]
     # assign that card choice to chosen card and add that to session list
-    chosen_card = session["current_hand"].pop(int(chosen_card_position))
-    session["chosen_cards"].append(chosen_card)
+    chosen_card = session["current_hand"].pop(int(choosen_card_position))
+    session["choosen_cards"].append(chosen_card)
 
     # add rest of hand to facedown cards
     if session["current_deck"] == "sprint":
@@ -370,7 +177,7 @@ def card_picker_2():
         deck_for_title = "Roller"
     else:
         deck_for_title = "Sprinter"
-    previous_card = session["chosen_cards"][0]
+    previous_card = session["choosen_cards"][0]
     session.modified = True
     return render_template(
         "card_picker_2.html",
@@ -383,10 +190,10 @@ def card_picker_2():
 @app.route("/hidden_cards", methods=["POST", "GET"])
 def hidden_cards():
     # get card choice from last page
-    chosen_card_position = request.form["card_choice"]
+    choosen_card_position = request.form["card_choice"]
     # assign that card choice to chosen card and add that to session list
-    chosen_card = session["current_hand"].pop(int(chosen_card_position))
-    session["chosen_cards"].append(chosen_card)
+    chosen_card = session["current_hand"].pop(int(choosen_card_position))
+    session["choosen_cards"].append(chosen_card)
 
     # add rest of hand to facedown cards
     if session["current_deck"] == "sprint":
@@ -405,15 +212,16 @@ def hidden_cards():
 
 @app.route("/revealed_cards/")
 def revealed_cards():
-    chosen_cards = session["chosen_cards"]
+    choosen_cards = session["choosen_cards"]
     next_round = int(session["round"]) + 1
     return render_template(
-        "revealed_cards.html", chosen_cards=chosen_cards, next_round=next_round
+        "revealed_cards.html", choosen_cards=choosen_cards, next_round=next_round
     )
 
 
 @app.route("/add_exhaustion/<deck>")
 def add_exhaustion(deck):
+    print(deck)
     if deck == "sprint":
         session["sprint_faceup"].append([2, "S", "exhaustion-card"])
         # flash("Exhaustion card added to Sprinter Deck")
